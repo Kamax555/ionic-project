6225471c8bbe38783ab0d1688fce6c8b5b1d736f
Rita <t3lixi00@students.oamk.fi>
2017-02-05 17:51:23 +0200

    search tab coding

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,13 +20,13 @@ angular.module('app.controllers', [])
         }
     })
 
-.controller('homePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('homePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function ($scope, $stateParams) {
 
 
-}])
+    }])
 
 .controller('searchTabCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -69,5 +69,5 @@ function ($scope, $stateParams) {
 }])
 
 ;
- 
+
 
