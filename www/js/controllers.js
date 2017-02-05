55a25fa3554729f0131475c2357e848ff9a4fd35
Kamill <t5sika00@students.oamk.fi>
2017-02-05 12:10:43 +0200

    Added controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,25 @@
 angular.module('app.controllers', [])
-  
+
+    .controller('HomeCtrl', function ($scope) {
+        $scope.posts = [];
+        $scope.showPosts = function () {
+            for (var i = 0; i < 20; i++) {
+                $scope.posts.push({
+                    id: i
+                    , avatar: "http://trump.frost.works/images/avatar.png"
+                    , username: "DonaldTrump"
+                    , picture: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
+                    , caption: "DonaldTrump I will make America great again!"
+                    , commentsNumber: 20
+                    , like: 1000
+                    , commenter: "TheLegend27"
+                    , comment: "We have the best president!"
+                    , postedTime: 20
+                });
+            }
+        }
+    })
+
 .controller('homePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -7,7 +27,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('searchTabCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -15,7 +35,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('addTabCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -23,7 +43,7 @@ function ($scope, $stateParams) {
 
 
 }])
-      
+
 .controller('profileTabCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -31,7 +51,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('commentsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -39,7 +59,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -47,4 +67,7 @@ function ($scope, $stateParams) {
 
 
 }])
+
+;
  
+
