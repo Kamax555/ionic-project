a9d36a5471858aa0adfbc51f5b00c75b3d9f1b5e
Kamill <t5sika00@students.oamk.fi>
2017-02-08 23:34:59 +0200

    Added "Select from gallery" option to add picture page

--- www/js/app.js
+++ www/js/app.js
@@ -95,9 +95,10 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
             targetWidth: 300,
             targetHeight: 300,
             popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
+            saveToPhotoAlbum: true
         };
  
+        
         $cordovaCamera.getPicture(options).then(function(imageData) {
             $scope.imgURI = "data:image/jpeg;base64," + imageData;
         }, function(err) {
@@ -105,4 +106,29 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
         });
     }
  
-});
+})
+
+.controller("GalleryController", function($scope, $cordovaCamera) {
+ 
+    $scope.selectGallery = function() {
+        var options = { 
+            quality : 75, 
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.PHOTOLIBRARY, 
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: true
+        };
+ 
+        
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // An error occured. Show a message to the user
+        });
+    }
+ 
+})
